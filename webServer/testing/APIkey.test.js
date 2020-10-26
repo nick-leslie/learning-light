8bf4c093d1aa5fc2e34bb33e9b7de8871c73046b
swordcom367 <46449945+swordcom367@users.noreply.github.com>
2020-10-26 08:37:29 -0400

    fixed testing

--- webServer/testing/APIkey.test.js
+++ webServer/testing/APIkey.test.js
@@ -5,5 +5,5 @@ test("testing api key verification and distibutuion",() => {
     let key = apiKeyDistributer.addNewKey("tester");
     console.log(key);
     key = apiKeyDecoder.verifyFunc(key);
-    expect(key).toStrictEqual({ teacher: 'tester', iat: Math.floor(+new Date() / 1000), exp: Math.floor(+new Date() / 1000)+86400, "permition": 1 });
+    expect(key).toStrictEqual({ teacher: 'tester', iat: Math.floor(+new Date() / 1000), exp: Math.floor(+new Date() / 1000)+86400});
 })
