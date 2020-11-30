3b0f201e92b732dcaab8423af1c98ef059ebd81f
swordcom367 <46449945+swordcom367@users.noreply.github.com>
2020-11-29 22:46:54 -0500

    fixed test by changeing input values

--- webServer/testing/roomState.test.js
+++ webServer/testing/roomState.test.js
@@ -10,7 +10,7 @@ test('testing populating room keys and then grabing a teacher from a room key', 
     expect(keys[listAllProperties(keys)[0]]).toStrictEqual(teacher);
 });
 test('testing the grab teacher from mac function', () => {
-    expect(roomState.grabTeacherFromMac('c8:2b:96:30:09:e9')).toStrictEqual('crossman')
+    expect(roomState.grabTeacherFromMac('C8:2B:96:30:32:BB')).toStrictEqual('crossman')
 });
 function listAllProperties(o) {
     var objectToInspect;     
